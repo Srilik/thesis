84fe97cde477d7086ab730b73c9ef6ccde9978e6
YONGkyy <yongkeo9@gmail.com>
2024-04-01 15:42:24 +0700

    advisor create

--- app/Http/Controllers/ThesisAdvisorController.php
+++ app/Http/Controllers/ThesisAdvisorController.php
@@ -8,28 +8,54 @@ use Inertia\Inertia;
 
 class ThesisAdvisorController extends Controller
 {
-    public function index(Request $request)
+
+    public function index()
     {
+        //
         $thesisAdvisors = ThesisAdvisor::query()
-            ->when($request->input('keyword'), fn ($query)
-            =>$query->where('Academic_Year', 'like', "%" . $request->input('keyword') . "%"))
-            ->paginate(5)
-            ->withQueryString();
+            ->orderBy('Academic_Year', 'desc')
+            ->when(request()->input('academic_year'), fn ($query)
+            =>$query->where('Academic_Year', request()->input('academic_year')))
+            ->when(request()->input('advisor'), fn ($query) =>
+            $query->where('Advisor', request()->input('advisor')))
+            ->when(request()->input('department'), fn ($query) =>
+            $query->where('Department', request()->input('department')))
+            ->paginate(5);
+        $academic_years = ThesisAdvisor::query()
+            ->selectRaw('Academic_Year')
+            ->distinct()
+            ->orderBy('Academic_Year', 'desc')
+            ->pluck('Academic_Year');
+
+        $advisors = ThesisAdvisor::query()
+            ->selectRaw('Advisor')
+            ->distinct()
+            ->orderBy('Advisor', 'asc')
+            ->pluck('Advisor');
+
+        $departments = ThesisAdvisor::query()
+            ->selectRaw('Department')
+            ->distinct()
+            ->orderBy('Department', 'asc')
+            ->pluck('Department');
+
+
 
         return Inertia::render('ThesisAdvisor/Index', [
-            'thesisAdvisors' =>$thesisAdvisors,
-            'filters' => $request->all('keyword'),
+            'thesisAdvisors' => $thesisAdvisors,
+            'academic_options' => $academic_years,
+            'advisor_options' => $advisors,
+            'department_options' => $departments,
+            'filters' => request()->all('academic_year', 'advisor', 'department')
         ]);
     }
-
     public function create()
-    { 
+    {
         return Inertia::render('ThesisAdvisor/Index');
     }
 
-    public function store(Request $request)
+    public function store(Request $request, $id = null)
     {
-
         // Validate the incoming request data
         $validatedData = $request->validate([
             "Academic_Year" => "required",
@@ -38,50 +64,26 @@ class ThesisAdvisorController extends Controller
             "Department" => "required",
         ]);
 
-
-        // // // Create a new Thesis instance
-        // $thesisAdvisor = new ThesisAdvisor();
-
-        // // // Assign values from the validated data to the Thesis model instance
-        // $thesisAdvisor->Academic_Year = $validatedData['Academic_Year'];
-        // $thesisAdvisor->Advisor = $validatedData['Advisor'];
-        // $thesisAdvisor->College = $validatedData['College'];
-        // $thesisAdvisor->Department = $validatedData['Department'];
-
-        // $thesisAdvisor->save();
-
-        if($request->input('id')){
-            $thesisAdvisors = ThesisAdvisor::findOrFail($request->input("id"));
-            $thesisAdvisors->update($validatedData);
-        }else {      
-
+        if ($id) {
+            $thesisAdvisor = ThesisAdvisor::findOrFail($id);
+            $thesisAdvisor->update($validatedData);
+        } else {
             ThesisAdvisor::create($validatedData);
         }
+
         return redirect()->route('thesisAdvisor.index');
     }
 
-    public function show(string $id)
+
+    public function edit(string $id)
     {
-        //
+        return ThesisAdvisor::findOrFail($id);
     }
 
-    public function edit(ThesisAdvisor $thesisAdvisor)
-    {
-        return Inertia::render('ThesisAdvisor/Index', [
-            'thesisAdvisor' =>$thesisAdvisor
-        ]);
-    }
-
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    public function destroy($id)
+    public function destroy(string $id)
     {
         $thesisAdvisor = ThesisAdvisor::findOrFail($id);
         $thesisAdvisor->delete();
-        return redirect()->back();    
+        return redirect()->back();
     }
 }
-
--- resources/js/Pages/ThesisAdvisor/Index.vue
+++ resources/js/Pages/ThesisAdvisor/Index.vue
@@ -3,15 +3,23 @@ import App from "@/Layouts/App.vue";
 import { PaginateType } from "@/types/paginateType";
 import { ThesisAdvisorType } from "@/types/thesisAdvisor";
 import { router, useForm } from "@inertiajs/vue3";
-import { pickBy, throttle } from "lodash";
+import { pickBy, throttle, values } from "lodash";
 import Swal from "sweetalert2";
 import { watch } from "vue";
+import Modal from "@/Components/Modal.vue";
+import axios from "axios";
+import { ref } from "vue";
 
 const props = defineProps<{
     thesisAdvisor?: ThesisAdvisorType;
     thesisAdvisors: PaginateType<ThesisAdvisorType>;
+    academic_options?: string[];
+    advisor_options?: string[];
+    department_options?: string[];
     filters?: {
-        keyword: string;
+        academic_Year?: string | null;
+        advisor?: string | null;
+        department?: string | null;
     };
 }>();
 
@@ -21,44 +29,52 @@ const form = useForm({
     College: props.thesisAdvisor?.College ?? "",
     Department: props.thesisAdvisor?.Department ?? "",
 });
+
+const onModal = ref(false);
+
+const onOpenModal = () => {
+    onModal.value = true;
+};
+
 const onSave = () => {
     // console.log(form.data());
     form.post(route("thesisAdvisor.store"), {
         onSuccess: () => {
             Swal.fire({
                 icon: "success",
-                title: "Advisor has been saved.",
-                timer: 2000,
+                title: "ThesisAdvisor has been saved.",
+                timer: 3000,
                 position: "top-end",
                 toast: true,
-                showConfirmButton: false,
             });
         },
     });
+    form.reset();
 };
 
 const filterForm = useForm({
-    keyword: props.filters?.keyword ?? "",
+    academic_year: props.filters?.academic_Year ?? "",
+    advisor: props.filters?.advisor ?? "",
+    department: props.filters?.department ?? "",
 });
 
 watch(
     () => filterForm.data(),
     throttle(() => {
-        console.log("log data");
         router.get(route("thesisAdvisor.index"), pickBy(filterForm.data()), {
             preserveState: true,
-            only: ["thesisAdvisors"],
+            only: ["thesisAdvisor"],
             replace: true,
         });
     }, 500),
 );
 
 const onClearFilter = () => {
-    filterForm.keyword = "";
+    filterForm.reset();
 };
 
-const onDelete = async (id: number) => {
-    await Swal.fire({
+const onDelete = (id: number) => {
+    Swal.fire({
         title: "Do you want to delete?",
         showDenyButton: false,
         showCancelButton: true,
@@ -73,107 +89,129 @@ const onDelete = async (id: number) => {
                     Swal.fire({
                         icon: "success",
                         text: "Deleted successfully!",
-                        title: "Advisor has been deleted.",
+                        title: "thesisAdvisor has been deleted.",
                         toast: true,
-                        position: "top-end",
-                        timer: 2000,
-                        showConfirmButton: false,
+                        timer: 3000,
                     });
                 },
             });
         }
     });
 };
+const onEdit = async (id: number) => {
+    const { data } = await axios.get(route("thesisAdvisor.edit", id));
+    form.Academic_Year = data.Academic_Year;
+    form.Advisor = data.Advisor;
+    form.College = data.College;
+    form.Department = data.Department;
+    onModal.value = true;
+};
 
-const onEdit = (id: number) => {
-    router.get(route("thesisAdvisor.edit", id));
-    form.Academic_Year = props.thesisAdvisor?.Academic_Year ?? "";
-    form.Advisor = props.thesisAdvisor?.Advisor ?? "";
-    form.Department = props.thesisAdvisor?.Department ?? "";
+const oncloseModal = () => {
+    onModal.value = false;
 };
 </script>
 
 <template>
-    <App>
+    <Modal :show="onModal" maxWidth="2xl">
         <div class="p-3">
             <h2 class="text-2xl font-bold">Create a ThesisAdvisor</h2>
             <div class="mt-4"></div>
             <div class="mt-4 p-4 bg-base-100 rounded-xl">
                 <form @submit.prevent="onSave">
-                    <div class="flex flex-col gap-2 lg:flex-row">
-                        <div class="flex flex-col w-full">
-                            <!-- <label class="label">Academic year</label> -->
-                            <input
-                                type="text"
-                                v-model="form.Academic_Year"
-                                placeholder="Academic Year"
-                                className="input input-bordered input-info w-full max-w-xs"
-                            />
-                            <!-- <input v-model="form.Academic_Year" class="input input-primary w-full"
-                                :class="{ 'input-error': form.errors.Academic_Year }" /> -->
-                            <div
-                                v-if="form.errors.Academic_Year"
-                                class="text-error"
-                            >
-                                {{ form.errors.Academic_Year }}
+                    <div class="flex flex-col w-full">
+                        <div>
+                            <div class="flex flex-col w-full">
+                                <label class="label">Academic year</label>
+                                <input
+                                    type="text"
+                                    v-model="form.Academic_Year"
+                                    placeholder="Academic Year"
+                                    class="input input-primary w-full"
+                                    :class="{
+                                        'input-error':
+                                            form.errors.Academic_Year,
+                                    }"
+                                />
+                                <div
+                                    v-if="form.errors.Academic_Year"
+                                    class="text-error"
+                                >
+                                    {{ form.errors.Academic_Year }}
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="flex flex-col w-full">
-                            <!-- <label class="label">Advisor</label> -->
-                            <input
-                                type="text"
-                                v-model="form.Advisor"
-                                placeholder="Advisor"
-                                className="input input-bordered input-info w-full max-w-xs"
-                            />
-                            <!-- <input v-model="form.Advisor" class="input input-primary w-full"
-                                :class="{ 'input-error': form.errors.Advisor }" /> -->
-                            <div v-if="form.errors.Advisor" class="text-error">
-                                {{ form.errors.Advisor }}
+                            <div class="flex flex-col w-full">
+                                <label class="label">Advisor</label>
+                                <input
+                                    type="text"
+                                    v-model="form.Advisor"
+                                    placeholder="Advisor"
+                                    class="input input-primary w-full"
+                                    :class="{
+                                        'input-error': form.errors.Advisor,
+                                    }"
+                                />
+                                <div
+                                    v-if="form.errors.Advisor"
+                                    class="text-error"
+                                >
+                                    {{ form.errors.Advisor }}
+                                </div>
                             </div>
                         </div>
-
-                        <div class="flex flex-col w-full">
-                            <!-- <label class="label">College</label> -->
-                            <input
-                                type="text"
-                                v-model="form.College"
-                                placeholder="College"
-                                className="input input-bordered input-info w-full max-w-xs"
-                            />
-                            <!-- <input v-model="form.College" class="input input-primary w-full"
-                                :class="{ 'input-error': form.errors.College }" /> -->
-                            <div v-if="form.errors.College" class="text-error">
-                                {{ form.errors.College }}
+                        <div>
+                            <div class="flex flex-col w-full">
+                                <label class="label">College</label>
+                                <input
+                                    type="text"
+                                    v-model="form.College"
+                                    placeholder="College"
+                                    class="input input-primary w-full"
+                                    :class="{
+                                        'input-error': form.errors.College,
+                                    }"
+                                />
+                                <div
+                                    v-if="form.errors.College"
+                                    class="text-error"
+                                >
+                                    {{ form.errors.College }}
+                                </div>
                             </div>
-                        </div>
-                        <div class="flex flex-col w-full">
-                            <!-- <label class="label">Department</label> -->
-                            <input
-                                type="text"
-                                v-model="form.Department"
-                                placeholder="Department"
-                                className="input input-bordered input-info w-full max-w-xs"
-                            />
-                            <!-- <input v-model="form.Department" class="input input-primary w-full"
-                                :class="{ 'input-error': form.errors.Department }" /> -->
-                            <div
-                                v-if="form.errors.Department"
-                                class="text-error"
-                            >
-                                {{ form.errors.Department }}
+                            <div class="flex flex-col w-full">
+                                <label class="label">Department</label>
+                                <input
+                                    type="text"
+                                    v-model="form.Department"
+                                    placeholder="Department"
+                                    class="input input-primary w-full"
+                                    :class="{
+                                        'input-error': form.errors.Department,
+                                    }"
+                                />
+                                <div
+                                    v-if="form.errors.Department"
+                                    class="text-error"
+                                >
+                                    {{ form.errors.Department }}
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="mt-2 flex justify-end">
+                    <div class="mt-2 flex justify-end gap-3">
+                        <button class="btn btn-warning" @click="oncloseModal">
+                            Close
+                        </button>
                         <button type="submit" class="btn btn-success">
-                            Save
+                            {{ form.isDirty ? 'Update' : 'Save' }}
                         </button>
                     </div>
                 </form>
             </div>
         </div>
+    </Modal>
+    <App>
         <div class="p-3">
             <div class="mb-2">
                 <h2 class="text-2xl font-bold">ThesisAdvisor Management</h2>
@@ -181,13 +219,48 @@ const onEdit = (id: number) => {
                     <div
                         class="bg-base-100 p-2 rounded-xl flex gap-2 items-center"
                     >
-                        <!-- <Link :href="route('thesisAdvisor.create')" class="btn btn-info">New</Link> -->
-                        <input
-                            v-model="filterForm.keyword"
-                            type="text"
-                            placeholder="Search by Academic_Year...."
-                            class="input input-info w-full"
-                        />
+                        <button class="btn btn-primary" @click="onOpenModal">
+                            New
+                        </button>
+
+                        <select
+                            v-model="filterForm.academic_year"
+                            class="select select-info w-full"
+                        >
+                            <option value="">All Years</option>
+                            <option
+                                v-for="years in academic_options"
+                                :value="years"
+                            >
+                                {{ years }}
+                            </option>
+                        </select>
+
+                        <select
+                            v-model="filterForm.advisor"
+                            class="select select-info w-full"
+                        >
+                            <option value="">All Advisor Name</option>
+                            <option
+                                v-for="advisors in advisor_options"
+                                :value="advisors"
+                            >
+                                {{ advisors }}
+                            </option>
+                        </select>
+
+                        <select
+                            v-model="filterForm.department"
+                            class="select select-info w-full"
+                        >
+                            <option value="">All Department Name</option>
+                            <option
+                                v-for="departments in department_options"
+                                :value="departments"
+                            >
+                                {{ departments }}
+                            </option>
+                        </select>
 
                         <button
                             class="btn btn-warning"
@@ -200,23 +273,23 @@ const onEdit = (id: number) => {
                 </div>
             </div>
             <div class="bg-base-100 rounded-xl overflow-x-auto">
-                <table class="table table-2xl">
+                <table class="table">
                     <thead>
-                        <tr>
+                        <tr class="text uppercase text-sm">
                             <th>Id</th>
-                            <th>Academic_Year</th>
+                            <th>Academic</th>
                             <th>Advisor</th>
                             <th>College</th>
                             <th>Department</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody v-if="thesisAdvisors && thesisAdvisors.data">
                         <tr
                             v-for="(item, index) in thesisAdvisors.data"
                             :key="index"
                         >
-                            <td>{{ item.id }}</td>
+                            <td>{{ index + 1 }}</td>
                             <td>{{ item.Academic_Year }}</td>
                             <td>{{ item.Advisor }}</td>
                             <td>{{ item.College }}</td>
@@ -245,14 +318,14 @@ const onEdit = (id: number) => {
             <!-- Pagination -->
             <div class="bg-base-100 rounded-xl mt-2 flex justify-center p-2">
                 <div class="join">
-                    <Link
+                    <b
                         v-for="link in thesisAdvisors.links"
                         :href="link.url ?? '#'"
                         class="join-item btn"
                         :class="{ 'btn-info': link.active }"
                     >
                         <span v-html="link.label"></span>
-                    </Link>
+                    </b>
                 </div>
             </div>
         </div>
