f14d3aa59e741db15b0a348abf457468af9f7a07
YONGkyy <yongkeo9@gmail.com>
2024-04-04 11:32:01 +0700

    update UI validation update function

--- app/Http/Controllers/ThesisAdvisorController.php
+++ app/Http/Controllers/ThesisAdvisorController.php
@@ -3,39 +3,75 @@
 namespace App\Http\Controllers;
 
 use App\Models\ThesisAdvisor;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ThesisAdvisorController extends Controller
 {
-    // public function index(Request $request)
-    // {
-    //     $thesisAdvisors = ThesisAdvisor::query()
-    //         ->when($request->input('keyword'), fn ($query)
-    //         => $query->where('Academic_Year', 'like', "%" . $request->input('keyword') . "%"))
-    //         ->paginate(5)
-    //         ->withQueryString();
 
-    //     return Inertia::render('ThesisAdvisor/Index', [
-    //         'thesisAdvisors' => $thesisAdvisors,
-    //         'filters' => $request->all('keyword'),
-    //     ]);
-    // }
-
-    public function index(Request $request)
+    public function index()
     {
-        //
+        // $thesisAdvisors = ThesisAdvisor::query()
+        //     ->orderBy('Academic_Year', 'desc')
+        //     ->when(request()->input('academic_year'), fn ($query) =>
+        //     $query->where('Academic_Year', request()->input('academic_year')))
+        //     ->when(request()->input('advisor'), fn ($query) =>
+        //     $query->where('Advisor', request()->input('advisor')))
+        //     ->when(request()->input('department'), fn ($query) =>
+        //     $query->where('Department', request()->input('department')))
+        //     ->paginate(2);
         $thesisAdvisors = ThesisAdvisor::query()
             ->orderBy('Academic_Year', 'desc')
-            ->when($request->input('keyword'), fn ($query)
-            => $query->where('Academic_Year', 'like', '%' . $request->input('keyword') . '%'))
-            ->paginate(5);
+            ->when(
+                request()->input('academic_year'),
+                fn ($query) =>
+                $query->where('Academic_Year', request()->input('academic_year'))
+            )
+            ->when(
+                request()->input('advisor'),
+                fn ($query) =>
+                $query->where('Advisor', request()->input('advisor'))
+            )
+            ->when(
+                request()->input('department'),
+                fn ($query) =>
+                $query->where('Department', request()->input('department'))
+            )->paginate(10);
 
+
+
+
+        $academic_years = ThesisAdvisor::query()
+            ->selectRaw('Academic_Year')
+            ->distinct()
+            ->orderBy('Academic_Year', 'desc')
+            ->pluck('Academic_Year');
+
+        $advisors = ThesisAdvisor::query()
+            ->selectRaw('Advisor')
+            ->distinct()
+            ->orderBy('Advisor', 'asc')
+            ->pluck('Advisor');
+
+        $departments = ThesisAdvisor::query()
+            ->selectRaw('Department')
+            ->distinct()
+            ->orderBy('Department', 'asc')
+            ->pluck('Department');
+
+        // return $thesisAdvisors;
 
         return Inertia::render('ThesisAdvisor/Index', [
             'thesisAdvisors' => $thesisAdvisors,
+            'academic_options' => $academic_years,
+            'advisor_options' => $advisors,
+            'department_options' => $departments,
+            'filters' => request()->all('academic_year', 'advisor', 'department'),
         ]);
     }
+
+
     public function create()
     {
         return Inertia::render('ThesisAdvisor/Index');
@@ -43,24 +79,32 @@ class ThesisAdvisorController extends Controller
 
     public function store(Request $request, $id = null)
     {
+        // try {
         // Validate the incoming request data
         $validatedData = $request->validate([
             "Academic_Year" => "required",
-            "Advisor" =>  "required",
+            "Advisor" => "required",
             "College" =>  "required",
             "Department" => "required",
         ]);
 
-        if ($id) {
-            $thesisAdvisor = ThesisAdvisor::findOrFail($id);
-            $thesisAdvisor->update($validatedData);
-        } else {
-            ThesisAdvisor::create($validatedData);
-        }
+        $data = [
+            "Academic_Year" => $request->input('Academic_Year'),
+            "Advisor" => $request->input('Advisor'),
+            "College" => $request->input('College'),
+            "Department" => $request->input('Department'),
+        ];
+        // If $id is provided, update the existing record; otherwise, create a new record
+        $thesisAdvisor = ThesisAdvisor::updateOrCreate(['id' => $id], $data);
 
         return redirect()->route('thesisAdvisor.index');
+        // } catch (QueryException $e) {
+        //     if ($e->errorInfo[1] === 1062) { // 1062 is the MySQL error code for a duplicate entry
+        //         return redirect()->back()->withErrors(['Advisor' => 'The Advisor already exists.'])->withInput();
+        //     }
+        //     throw $e;
+        // }
     }
-
 
     public function edit(string $id)
     {
@@ -73,68 +117,4 @@ class ThesisAdvisorController extends Controller
         $thesisAdvisor->delete();
         return redirect()->back();
     }
-
-    // public function create()
-    // {
-    //     return Inertia::render('ThesisAdvisor/Index');
-    // }
-
-    // public function store(Request $request)
-    // {
-
-    //     // Validate the incoming request data
-    //     $validatedData = $request->validate([
-    //         "Academic_Year" => "required",
-    //         "Advisor" =>  "required",
-    //         "College" =>  "required",
-    //         "Department" => "required",
-    //     ]);
-
-
-    //     // // // Create a new Thesis instance
-    //     // $thesisAdvisor = new ThesisAdvisor();
-
-    //     // // // Assign values from the validated data to the Thesis model instance
-    //     // $thesisAdvisor->Academic_Year = $validatedData['Academic_Year'];
-    //     // $thesisAdvisor->Advisor = $validatedData['Advisor'];
-    //     // $thesisAdvisor->College = $validatedData['College'];
-    //     // $thesisAdvisor->Department = $validatedData['Department'];
-
-    //     // $thesisAdvisor->save();
-
-    //     if ($request->input('id')) {
-    //         $thesisAdvisors = ThesisAdvisor::findOrFail($request->input("id"));
-    //         $thesisAdvisors->update($validatedData);
-    //     } else {
-
-    //         ThesisAdvisor::create($validatedData);
-    //     }
-    //     return redirect()->route('thesisAdvisor.index');
-    // }
-
-    // public function show(string $id)
-    // {
-    //     //
-
-    // }
-
-    // public function edit(ThesisAdvisor $thesisAdvisor)
-    // {
-    //     // dd($thesisAdvisor);
-    //     return Inertia::render('ThesisAdvisor/Index', [
-    //         'thesisAdvisor' => $thesisAdvisor
-    //     ]);
-    // }
-
-    // public function update(Request $request, string $id)
-    // {
-    //     //
-    // }
-
-    // public function destroy($id)
-    // {
-    //     $thesisAdvisor = ThesisAdvisor::findOrFail($id);
-    //     $thesisAdvisor->delete();
-    //     return redirect()->back();
-    // }
 }
--- resources/js/Pages/ThesisAdvisor/Index.vue
+++ resources/js/Pages/ThesisAdvisor/Index.vue
@@ -3,69 +3,81 @@ import App from "@/Layouts/App.vue";
 import { PaginateType } from "@/types/paginateType";
 import { ThesisAdvisorType } from "@/types/thesisAdvisor";
 import { router, useForm } from "@inertiajs/vue3";
-import { pickBy, throttle } from "lodash";
+import { pick, pickBy, throttle } from "lodash";
 import Swal from "sweetalert2";
 import { watch } from "vue";
 import Modal from "@/Components/Modal.vue";
-import axios from "axios";
 import { ref } from "vue";
+import axios from "axios";
 
 const props = defineProps<{
     thesisAdvisor?: ThesisAdvisorType;
     thesisAdvisors: PaginateType<ThesisAdvisorType>;
+    academic_options?: string[];
+    advisor_options?: string[];
+    department_options?: string[];
     filters?: {
-        keyword: string;
+        academic_Year?: string | null;
+        advisor?: string | null;
+        department?: string | null;
     };
 }>();
 
 const form = useForm({
-    Academic_Year: props.thesisAdvisor?.Academic_Year ?? "",
-    Advisor: props.thesisAdvisor?.Advisor ?? "",
-    College: props.thesisAdvisor?.College ?? "",
-    Department: props.thesisAdvisor?.Department ?? "",
+    id: null,
+    Academic_Year: "",
+    Advisor: "",
+    College: "",
+    Department: "",
 });
+
+const onOpenModal = ref(false);
+
 const onSave = () => {
-    // console.log(form.data());
-    form.post(route("thesisAdvisor.store"), {
+    form.post(route("thesisAdvisor.store", form.id ?? "update"), {
         onSuccess: () => {
             Swal.fire({
                 icon: "success",
                 title: "ThesisAdvisor has been saved.",
-                timer: 3000,
+                timer: 1500,
                 position: "top-end",
                 toast: true,
             });
+            onOpenModal.value = false;
+            form.reset();
+            form.clearErrors();
         },
     });
 };
 
 const filterForm = useForm({
-    keyword: props.filters?.keyword ?? "",
+    academic_year: props.filters?.academic_Year ?? "",
+    advisor: props.filters?.advisor ?? "",
+    department: props.filters?.department ?? "",
 });
 
 watch(
     () => filterForm.data(),
     throttle(() => {
-        console.log("log data");
         router.get(route("thesisAdvisor.index"), pickBy(filterForm.data()), {
             preserveState: true,
-            only: ["thesisAdvisors"],
             replace: true,
         });
     }, 500),
 );
 
 const onClearFilter = () => {
-    filterForm.keyword = "";
+    filterForm.reset();
 };
 
 const onDelete = (id: number) => {
     Swal.fire({
         title: "Do you want to delete?",
+        icon: "warning",
         showDenyButton: false,
         showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
         confirmButtonText: "Delete",
         denyButtonText: "Cancel",
     }).then((result) => {
@@ -75,9 +87,11 @@ const onDelete = (id: number) => {
                     Swal.fire({
                         icon: "success",
                         text: "Deleted successfully!",
-                        title: "thesisAdvisor has been deleted.",
+                        title: "The Advisor has been deleted.",
+                        showConfirmButton: false,
                         toast: true,
-                        timer: 3000,
+                        timer: 1000,
+                        position: "top-end",
                     });
                 },
             });
@@ -86,29 +100,51 @@ const onDelete = (id: number) => {
 };
 const onEdit = async (id: number) => {
     const { data } = await axios.get(route("thesisAdvisor.edit", id));
+    form.id = data.id;
     form.Academic_Year = data.Academic_Year;
     form.Advisor = data.Advisor;
     form.College = data.College;
     form.Department = data.Department;
-};
-
-const onModal = ref(false);
-
-const onOpenModal = () => {
-    onModal.value = true;
+    onOpenModal.value = true;
 };
 
 const oncloseModal = () => {
-    onModal.value = false;
+    if (form.isDirty) {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "Do you want to close this form?",
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: "Yes",
+            denyButtonText: `No`,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                onOpenModal.value = false;
+                form.reset();
+                form.clearErrors();
+            }
+        });
+    } else {
+        onOpenModal.value = false;
+        form.reset();
+        form.clearErrors();
+    }
 };
 </script>
 
 <template>
-    <Modal :show="onModal" maxWidth="2xl" >
-        <div class="p-3">
-            <h2 class="text-2xl font-bold">Create a ThesisAdvisor</h2>
-            <div class="mt-4"></div>
-            <div class="mt-4 p-4 bg-base-100 rounded-xl">
+    <Modal :show="onOpenModal" maxWidth="2xl" @close="oncloseModal">
+        <div class="p-3 bg-base-200 rounded-xl">
+            <div class="text-center mt-2">
+                <h2 class="text-2xl font-bold">
+                    {{
+                        form.id
+                            ? "Thesis Advisor Update"
+                            : "Thesis Advisor Create"
+                    }}
+                </h2>
+            </div>
+            <div class="p-4 rounded-xl">
                 <form @submit.prevent="onSave">
                     <div class="flex flex-col w-full">
                         <div>
@@ -117,7 +153,6 @@ const oncloseModal = () => {
                                 <input
                                     type="text"
                                     v-model="form.Academic_Year"
-                                    placeholder="Academic Year"
                                     class="input input-primary w-full"
                                     :class="{
                                         'input-error':
@@ -137,7 +172,6 @@ const oncloseModal = () => {
                                 <input
                                     type="text"
                                     v-model="form.Advisor"
-                                    placeholder="Advisor"
                                     class="input input-primary w-full"
                                     :class="{
                                         'input-error': form.errors.Advisor,
@@ -157,7 +191,6 @@ const oncloseModal = () => {
                                 <input
                                     type="text"
                                     v-model="form.College"
-                                    placeholder="College"
                                     class="input input-primary w-full"
                                     :class="{
                                         'input-error': form.errors.College,
@@ -175,7 +208,6 @@ const oncloseModal = () => {
                                 <input
                                     type="text"
                                     v-model="form.Department"
-                                    placeholder="Department"
                                     class="input input-primary w-full"
                                     :class="{
                                         'input-error': form.errors.Department,
@@ -190,12 +222,16 @@ const oncloseModal = () => {
                             </div>
                         </div>
                     </div>
-                    <div class="mt-2 flex justify-end gap-3">
-                        <button class="btn btn-warning" @click="oncloseModal">
+                    <div class="mt-3 flex justify-end gap-4">
+                        <button
+                            type="button"
+                            class="btn btn-warning"
+                            @click="oncloseModal"
+                        >
                             Close
                         </button>
                         <button type="submit" class="btn btn-success">
-                            Save
+                            {{ form.id ? "Update" : "Save" }}
                         </button>
                     </div>
                 </form>
@@ -204,21 +240,67 @@ const oncloseModal = () => {
     </Modal>
     <App>
         <div class="p-3">
-            <button class="btn btn-primary" @click="onOpenModal">New</button>
             <div class="mb-2">
                 <h2 class="text-2xl font-bold">ThesisAdvisor Management</h2>
-                <div class="mt-4">
-                    <div
-                        class="bg-base-100 p-2 rounded-xl flex gap-2 items-center"
-                    >
-                        <!-- <Link :href="route('thesisAdvisor.create')" class="btn btn-info">New</Link> -->
-                        <input
-                            v-model="filterForm.keyword"
-                            type="text"
-                            placeholder="Search..."
-                            class="input input-info w-full"
-                        />
+                <div class="mt-2 bg-base-100 px-2 pb-2 rounded-xl flex gap-2">
+                    <div class="mt-auto">
+                        <button
+                            class="btn btn-primary align-bottom"
+                            @click="onOpenModal = true"
+                        >
+                            New
+                        </button>
+                    </div>
 
+                    <div class="flex flex-col w-full">
+                        <label class="label label-text">Academic Year</label>
+                        <select
+                            v-model="filterForm.academic_year"
+                            class="select select-info w-full"
+                        >
+                            <option value="">All Years</option>
+                            <option
+                                v-for="years in academic_options"
+                                :value="years"
+                            >
+                                {{ years }}
+                            </option>
+                        </select>
+                    </div>
+
+                    <div class="flex flex-col w-full">
+                        <label class="label label-text">Advisor Name</label>
+                        <select
+                            v-model="filterForm.advisor"
+                            class="select select-info w-full"
+                        >
+                            <option value="">All Advisors</option>
+                            <option
+                                v-for="advisors in advisor_options"
+                                :value="advisors"
+                            >
+                                {{ advisors }}
+                            </option>
+                        </select>
+                    </div>
+
+                    <div class="flex flex-col w-full">
+                        <label class="label label-text">Department</label>
+                        <select
+                            v-model="filterForm.department"
+                            class="select select-info w-full"
+                        >
+                            <option value="">All Departments</option>
+                            <option
+                                v-for="departments in department_options"
+                                :value="departments"
+                            >
+                                {{ departments }}
+                            </option>
+                        </select>
+                    </div>
+
+                    <div class="mt-auto">
                         <button
                             class="btn btn-warning"
                             type="button"
@@ -275,14 +357,14 @@ const oncloseModal = () => {
             <!-- Pagination -->
             <div class="bg-base-100 rounded-xl mt-2 flex justify-center p-2">
                 <div class="join">
-                    <b
+                    <Link
                         v-for="link in thesisAdvisors.links"
                         :href="link.url ?? '#'"
                         class="join-item btn"
                         :class="{ 'btn-info': link.active }"
                     >
                         <span v-html="link.label"></span>
-                    </b>
+                    </Link>
                 </div>
             </div>
         </div>
